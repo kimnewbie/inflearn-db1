4861242c741a06d17475c9dbc13a1d00146cf82e
김유진 <gracecancho@gmail.com>
2024-05-20 10:59:15 +0900

    DB 연결

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,11 @@
+package hello.jdbc.connection;
+
+/**
+ * 데이터베이스에 접속하는데 필요한 기본 정보를 편리하게 사용할 수 있도록 
+ * 상수로 생성
+ */
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
